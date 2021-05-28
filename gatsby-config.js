e8f98e7c7edd48bc024bb243747cd94ef3cd6221
Felikso <aleksandrasniegucka@wp.pl>
2021-05-28 20:03:47 +0200

    removed ne

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,6 +117,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     //`gatsby-plugin-offline`,
     `gatsby-plugin-minify`,
-    `gatsby-plugin-netlify-cache`,
   ],
 }
